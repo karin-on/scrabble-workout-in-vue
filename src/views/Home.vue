f22e4fe8d11e0c5119cbd811ba5dbb05bc0ab13b
Karina Nowicka <knowicka@gmail.com>
2020-09-02 21:07:13 +0200

    Setup project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,27 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+// import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
+// @Component({
+//   components: {
+//     HelloWorld,
+//   },
+// })
+// export default class Home extends Vue {
+//   mounted(): void {
+//     console.log('test');
+//   }
+// }
+
+export default {
+  name: 'Home',
   components: {
     HelloWorld,
   },
-})
-export default class Home extends Vue {}
+  mounted(): void {
+    console.log('test');
+  },
+};
 </script>
