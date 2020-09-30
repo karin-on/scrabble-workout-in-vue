9f48267fec0107f8323c7503a4d3ac807098e28d
Karina Nowicka <knowicka@gmail.com>
2020-09-30 22:19:13 +0200

    Add Submit component, showing & hiding submit buttons, navigating to result

---
+++ src/components/Submit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="submit">
+    <button
+      class="submit__submit-button button -small -filled"
+      @click="handleSubmitClick">Zatwierdź</button>
+    <button
+      class="submit__cancel-button button -small -outlined"
+      @click="handleCancelClick">Anuluj</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Submit extends Vue {
+  handleSubmitClick(): void {
+    this.$emit('submit');
+  }
+
+  handleCancelClick(): void {
+    this.$emit('cancel');
+  }
+}
+</script>
